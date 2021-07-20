998e227074f93d8c7653e4e273eecff9b86deb61
Johannes Lamers <johannes.lamers@tu-dortmund.de>
2021-07-20 20:16:26 +0200

    Myonen auswertung python angefangen

--- Myonen/plot.py
+++ Myonen/plot.py
@@ -12,7 +12,54 @@ from scipy.stats import sem
 def abw(exact,approx):
     return (exact-approx)*100/exact  #Abweichnung
 
+#verz ist die relative Verzögerung der PMP
+#I die Anzahl Impule pro Sekunde
+verz , I = np.genfromtxt('data/verz.txt', delimiter=',', unpack=True)
 
+def plat(x,h):
+    return 0*x+h
+
+I_err = np.sqrt(I)
+I_uarray = uarray(I, I_err)
+
+#Bereich des Plateaus
+anf = 11
+end = 22
+verz_lin = np.linspace(verz[anf],verz[end])
+
+params1, cov1 = curve_fit(plat, verz[anf:end], I[anf:end],sigma=I_err[anf:end],p0=[2])
+
+plt.figure()
+plt.errorbar(verz,I, yerr=I_err, fmt='_',capsize=3, label ='Messwerte')
+plt.plot(verz_lin, plat(verz_lin,*params1), ls = '--', label = 'Plateau')
+plt.grid()
+plt.xlabel('T_vz ns')
+plt.ylabel('Impulsrate')
+plt.legend()
+plt.show()
+
+# T ist T_vz in 0.1 micro sekunden
+# K ist die Nummer des Kanals
+T , K = np.genfromtxt('data/Tvz_kanal.txt', delimiter=',', unpack=True)
+
+T = np.array(T)
+T = T * 0.1
+print(T,K)
+K_lin = np.linspace(0, 100, 100)
+
+def gerade(x,a,b):
+   return a*x+b 
+params2, cov2 = curve_fit(gerade, K, T,p0=[1,1])
+
+perr2 = np.sqrt(np.diag(cov2))
+params_err2 = uarray(params2, perr2)
+plt.plot(K,T, 'kx', label = 'Messwerte')
+plt.plot(K, gerade(K,*params2), label = 'Ausgleichsgerade')
+plt.grid()
+plt.xlabel('Kanal')
+plt.ylabel('t in micro sek')
+plt.legend()
+plt.show()
 ##Curvefit
 #def BeispielFunktion(x,a,b):
 #    return a*x+b 

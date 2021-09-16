a26fa4c981ba647bded946d7729c4fff89200a11
Johannes Lamers <johannes.lamers@tu-dortmund.de>
2021-09-16 11:58:53 +0200

    Dipol, Diskuss

---
+++ Dipolrelaxation/abw.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+from uncertainties import ufloat
+from uncertainties.unumpy import uarray
+from uncertainties import unumpy as unp
+from uncertainties.unumpy import (nominal_values as noms,std_devs as stds)
+
+def abw(exact,approx):
+    return (exact-approx)*100/exact  #Abweichnung
+
+W_lit = 0.66 #eV
+
+W_1_15 = ufloat(0.91,0.04)
+W_1_2 =  ufloat(0.59,0.08)
+W_2_15 = ufloat(0.524,0.033)
+W_2_2 =  ufloat(0.560,0.033)
+
+
+print(abw(W_lit,W_1_15))
+print(abw(W_lit,W_1_2))
+print(abw(W_lit,W_2_15))
+print(abw(W_lit,W_2_2))
+
+
+t_lit = 4 * 10**(-14)
+t_1_15 = ufloat(2.5,29) * 10**(-12)
+t_1_2 =  ufloat(1.8,21) * 10**(-12)
+t_2_15 = ufloat(1.9,32) * 10**(-13)
+t_2_2=   ufloat(1.4,24) * 10**(-13)
+
+print(abw(t_lit, t_1_15))
+print(abw(t_lit, t_1_2))
+print(abw(t_lit, t_2_15))
+print(abw(t_lit, t_2_2))
